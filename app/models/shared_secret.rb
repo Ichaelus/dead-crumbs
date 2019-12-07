6d4ea873e23886d33df0c1077f3bd6882fc583fe
neonmate <neonmate@users.noreply.github.com>
2019-12-07 14:50:26 +0100

    Implement shared secret generator

--- app/models/shared_secret.rb
+++ app/models/shared_secret.rb
@@ -1,13 +1,60 @@
 # Usage:
 #
 # SharedSecret.create(number_of_required_shares: 3, number_of_total_shares: 5)
+# => ['1234', ['1---2468', '2---3702', '3---4936']]
+#
 # SharedSecret.recover(['aaa', 'bbb', 'ccc'])
 #
 
 class SharedSecret
 
-  def self.create(number_of_required_shares: 3, number_of_total_shares: 5)
-    number_of_total_shares.times.map { SecureRandom.hex(12) }
+  # 12th Mersenne Prime
+  RANDOM_POINT_THRESHOLD = 2**127 - 1
+
+  class Point
+    SEPARATOR = '---'.freeze
+
+    def self.deserialize(point)
+        x, y = point.split(SEPARATOR)
+        Point.new(x: x, y: y)
+    end
+
+    attr_accessor :x, :y
+
+    def initialize(x:, y:)
+      self.x = x
+      self.y = y
+    end
+
+    def serialize
+      "#{x}#{SEPARATOR}#{y}"
+    end
+  end
+
+  def self.create(number_of_required_shares:, number_of_total_shares:)
+    if number_of_required_shares > number_of_total_shares
+      raise ArgumentError, "Expected less than #{number_of_total_shares} required shares"
+    end
+
+    polynom = number_of_required_shares.times.map do
+      Random.rand(RANDOM_POINT_THRESHOLD)
+    end
+
+    points = (1..number_of_total_shares).map do |x_coordinate|
+      Point.new(x: x_coordinate, y: eval_polynom(polynom, x_coordinate)).serialize
+    end
+
+    [polynom[0].to_s, points]
+  end
+
+  def self.eval_polynom(polynom, x_coordinate)
+    sum = 0
+
+    polynom.each_with_index do |coefficient, index|
+      sum += coefficient * x_coordinate ** index
+    end
+
+    sum
   end
 
   def self.recover(shares)
--- spec/models/shared_secret_spec.rb
+++ spec/models/shared_secret_spec.rb
@@ -2,7 +2,43 @@ describe SharedSecret do
 
   describe '.create' do
     it 'creates the requested number of shared' do
-      expect(described_class.create(3, 5).size).to eq(5)
+      expect(described_class.create(number_of_required_shares: 3, number_of_total_shares: 5)[1].size).to eq(5)
+    end
+
+    it 'raises an error if a wrong number of required shares is provided' do
+      expect { described_class.create(number_of_required_shares: 6, number_of_total_shares: 5) }.to raise_error(
+        ArgumentError,
+        'Expected less than 5 required shares'
+      )
+    end
+
+    it 'creates a secret with its shares' do
+      allow(Random).to receive(:rand).and_return(1234)
+
+      expect(described_class.create(number_of_required_shares: 2, number_of_total_shares: 3)).to eq([
+        '1234',
+        [
+          '1---2468',
+          '2---3702',
+          '3---4936',
+        ]
+      ])
+    end
+  end
+
+  describe '.eval_polynom' do
+    it 'evaluates a polynom of first degree at point x' do
+      polynom = [5]
+
+      expect(described_class.eval_polynom(polynom, 2)).to eq(5)
+      expect(described_class.eval_polynom(polynom, 3)).to eq(5)
+    end
+
+    it 'evaluates a polynom of first degree at point x' do
+      polynom = [1, 1]
+
+      expect(described_class.eval_polynom(polynom, 2)).to eq(3)
+      expect(described_class.eval_polynom(polynom, 3)).to eq(4)
     end
   end
 
